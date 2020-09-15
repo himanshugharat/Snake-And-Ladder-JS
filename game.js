407a2b5e287ea720aa02392f4f20bbd3e32b7ec8
himanshu <himanshugharat44@gmail.com>
2020-09-15 06:29:07 -0500

    ADDED: the player option random for ladder snake or no play

--- game.js
+++ game.js
@@ -11,6 +11,9 @@
 //constant
 const startPosition = 0;
 
+//varables
+let position = startPosition;
+
 /**
  * @description: create a die roll function for 1 to 6 output
  * @returns: die roll
@@ -18,3 +21,35 @@ const startPosition = 0;
 function dieRoll() {
   return Math.floor((Math.random() * 10) % 6) + 1;
 }
+
+/**
+ * @description: create a randomOptionGenertor function
+ *               for ladder snake or normal move.
+ * @returns: random options
+ */
+function randomOptionGenerator() {
+  return Math.floor((Math.random() * 10) % 3) + 1;
+}
+
+/**
+ * @description: create a playerOptionCheck function
+ *               for nove places of ladder snake or normal move.
+ * @returns: die roll
+ */
+function playerOptionCheck() {
+  let options = randomOptionGenerator();
+  switch (options) {
+    case 1:
+      position = position + dieRoll();
+      break;
+    case 2:
+      let result = position - dieRoll();
+      if (result > 0) position = position - dieRoll();
+      else position = position;
+      break;
+    case 3:
+      position = position;
+  }
+}
+playerOptionCheck();
+console.log(position);
