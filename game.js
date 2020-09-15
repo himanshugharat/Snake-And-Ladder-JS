81eb5c6f5c513e5ccb6b39f49adf608f8d2b7c64
himanshu <himanshugharat44@gmail.com>
2020-09-15 06:09:56 -0500

    ADDED: the starting place of the player

---
+++ game.js
@@ -0,0 +1,12 @@
+/*
+ *Execution: default node cmd>node game.js
+ *Purpose  : To simulate the snake and ladder game
+ *@files   : game.js
+ *@overview: snake and ladder game simulation
+ *@author  : Himanshu Gharat
+ *@verson  : 1.0
+ *@since   : 15-09-2020
+ */
+
+//constant
+const startPosition = 0;
